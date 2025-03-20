275067fac70d8771fb182eb0b51b40c142f22846
Mark Hoemmen <mhoemmen@nvidia.com>
2025-03-20 23:59:17 +0200

    Add cpp-mdspan directory
    Build script is tested; build works.
    miniWeather_mdspan.cpp initially is just
    a copy of ../c/miniWeather_serial.cpp.

---
+++ cpp-mdspan/build/cmake-kermit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PNETCDF_ROOT=/raid/mhoemmen/pkg/pnetcdf-1.14.0
+SRC_ROOT=/raid/mhoemmen/src/miniWeather/cpp-mdspan
+
+LDFLAGS="-L${PNETCDF_ROOT}/lib -lpnetcdf" CXXFLAGS="-I${PNETCDF_ROOT}/include" cmake \
+  -DCMAKE_CXX_COMPILER=mpic++ \
+  -DCMAKE_C_COMPILER=mpicc \
+  -DCMAKE_Fortran_COMPILER=mpif90 \
+  ${SRC_ROOT}
